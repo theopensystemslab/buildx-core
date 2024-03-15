3cc0e7c5b4aea8d08d8bdd93aa323441908ac782
tom <tom@opensystemslab.io>
2024-03-15 08:39:00 +0000

    wip: raycasting working
    - outline effect composer setup needs attention with scene creation

--- examples/plural-caps/index.ts
+++ examples/plural-caps/index.ts
@@ -4,8 +4,62 @@ import defaultoryTask from "@/tasks/defaultory";
 import { createGridGroup } from "@/three/objects/GridGroup";
 import { A, O, T, TO, pipeLog } from "@/utils/functions";
 import { flow, pipe } from "fp-ts/lib/function";
+import { Object3D, Raycaster, Vector2, WebGLRenderer } from "three";
+import { EffectComposer, OutlinePass, RenderPass } from "three-stdlib";
 
-const { addObjectToScene } = createBasicScene();
+const renderer = new WebGLRenderer();
+
+const composer = new EffectComposer(renderer);
+
+const { addObjectToScene, scene, camera } = createBasicScene();
+
+const renderPass = new RenderPass(scene, camera);
+composer.addPass(renderPass);
+
+const outlinePass = new OutlinePass(
+  new Vector2(window.innerWidth, window.innerHeight),
+  scene,
+  camera
+);
+
+composer.addPass(outlinePass);
+
+renderer.domElement.addEventListener("pointermove", onPointerMove);
+
+const raycaster = new Raycaster();
+
+let selectedObjects: Object3D[] = [];
+
+const mouse = new Vector2();
+
+function onPointerMove(event: any) {
+  if (event.isPrimary === false) return;
+
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  checkIntersection();
+}
+
+function addSelectedObject(object: Object3D) {
+  selectedObjects = [];
+  selectedObjects.push(object);
+}
+
+function checkIntersection() {
+  raycaster.setFromCamera(mouse, camera);
+
+  const intersects = raycaster.intersectObject(scene, true);
+
+  if (intersects.length > 0) {
+    console.log(intersects.length);
+    const selectedObject = intersects[0].object;
+    addSelectedObject(selectedObject);
+    outlinePass.selectedObjects = selectedObjects;
+  } else {
+    // outlinePass.selectedObjects = [];
+  }
+}
 
 pipe(
   // sequenceT(T.ApplicativePar)(houseTypesTask, modulesTask),
